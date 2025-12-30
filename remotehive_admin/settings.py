7387e3bc3a7776064d629208757ea766303a7210
remotehive-dev <remotehive.official@gmail.com>
2025-12-30 10:37:36 +0530

    Fix settings: Add missing STATIC_ROOT

--- remotehive_admin/settings.py
+++ remotehive_admin/settings.py
@@ -178,6 +178,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STATICFILES_DIRS = [
     BASE_DIR / "backend_django" / "static",
 ]
